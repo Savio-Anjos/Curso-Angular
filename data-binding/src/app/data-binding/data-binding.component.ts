8b19db8f421bc3349adb572c6057d4a350e54ac0
savio <savioanjos2020@gmail.com>
2023-06-14 16:01:10 -0300

    :sparkles: feat: Property Binding e Interpolação

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `,
+  ],
+})
+export class DataBindingComponent implements OnInit {
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
+  nomeDoCurso: string = 'Angular';
+
+  valorInicial = 15;
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+}
